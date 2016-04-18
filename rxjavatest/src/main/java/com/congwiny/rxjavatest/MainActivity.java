aa8acc49093e623a1e42b3c5b39241134c79f68a
congwiny <congwiny@qq.com>
2016-04-18 10:28:23 +0800

    BugFix:fix gradle build error

--- rxjavatest/src/main/java/com/congwiny/rxjavatest/MainActivity.java
+++ rxjavatest/src/main/java/com/congwiny/rxjavatest/MainActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.graphics.drawable.DrawableUtils;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
