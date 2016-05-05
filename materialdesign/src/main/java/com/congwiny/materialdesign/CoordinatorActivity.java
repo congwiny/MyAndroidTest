b14884483070d7a39ddfff8eb512eaaf3dcbf438
congwiny <congwiny@qq.com>
2016-05-05 14:22:42 +0800

    modify:use toolbar replace actionbar

---
+++ materialdesign/src/main/java/com/congwiny/materialdesign/CoordinatorActivity.java
@@ -0,0 +1,9 @@
+package com.congwiny.materialdesign;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by congwiny on 2016/5/5.
+ */
+public class CoordinatorActivity extends AppCompatActivity {
+}
